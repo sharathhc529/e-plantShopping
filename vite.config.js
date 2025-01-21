e8e711aeb7d613cc6aa0d44f455e2bb60e45a4ad
Sharath Chandra <sharathhc529@gmail.com>
2025-01-21 20:54:55 +0530

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist'  // This is the default directory for build output
+  },
   base: "/shoppingreact",
   plugins: [react()],
 })
