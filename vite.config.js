6af4802747b62058d7c255b947a0054455ea73c5
Sharath Chandra <sharathhc529@gmail.com>
2025-01-22 14:49:05 +0530

    config corrections

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lush-green/",
-  resolve: {
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
-  }
+  base: "/lush-green"
 })
